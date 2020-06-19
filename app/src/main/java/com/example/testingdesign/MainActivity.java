9778eab579507c8c265d75717e6fb5a5bc1ad2e9
rushikesh <rushikeshsawant3108@gmail.com>
2020-06-19 21:52:23 +0530

    final design

---
+++ app/src/main/java/com/example/testingdesign/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.testingdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialButton login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void next(View view) {
+        Intent intent=new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
+}
